214b2b4eaf2896e6b1705e52bcfda96238853ff9
Akhil <aksompalli@gmail.com>
2018-02-14 15:40:37 -0800

    twitter omdb and spotify apis are working

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
+var Twitter = require('twitter');
 
+var name = process.argv[2]
 
-function twitter(){
-    var Twitter = require('twitter');
+var twitterClient = new Twitter({
+  consumer_key: 'M39K6oj5hx7c1DtE9I2KJNYWL',
+  consumer_secret: '4BhKpluyTriEJIq6Y14NedlsOBZhfqgVlqy5pXHRd0UwrjmcuY',
+  access_token_key: '240497888-BhhrWyyzMSptodKdHRiNBum3ziD8KdVUX0fstyHa',
+  access_token_secret: 'rWiUAz9Wmrmmo76Y91XSOk6E2c1wNQ5aGYuMrM3GlDblw'
+});
 
-    var client = new Twitter({
-      consumer_key: 'M39K6oj5hx7c1DtE9I2KJNYWL',
-      consumer_secret: '4BhKpluyTriEJIq6Y14NedlsOBZhfqgVlqy5pXHRd0UwrjmcuY',
-      access_token_key: '240497888-BhhrWyyzMSptodKdHRiNBum3ziD8KdVUX0fstyHa',
-      access_token_secret: 'rWiUAz9Wmrmmo76Y91XSOk6E2c1wNQ5aGYuMrM3GlDblw'
-    });
+var Spotify = require('node-spotify-api');
+     
+var spotify = new Spotify({
+  id: "0e7736924c084d1db7f7a500ad0b6da3",
+  secret:"a62f853c5dc24e468e3c1b8f18caa484"
+});
 
-    var handle = process.argv[2];
-    var params = {screen_name: handle};
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+var request = require("request");
+
+function getTweets(){
+    var params = {screen_name: "billburr"};
+    twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
       if (!error) {
           for ( var i = 0; i < tweets.length; i++){
                 console.log(tweets[i].text);
@@ -25,34 +33,103 @@ function twitter(){
     });
 }
 
-function omdb(){
- 
-    var request = require("request");
-    var nodeArgs = process.argv;
-    var movieName = "";
-    for (var i = 2; i < nodeArgs.length; i++) {
-      if (i > 2 && i < nodeArgs.length) {
-        movieName = movieName + "+" + nodeArgs[i];
-      }
-      else {
-        movieName += nodeArgs[i];
-      }
-    }
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+//twitter()
 
-    request(queryUrl, function(error, response, body) {
+function getMovieInfo(){
+  var nodeArgs = process.argv
+  var movieName = stringBuilder()
+  console.log(movieName)
+    
+  if(!movieName){
+
+    movieName = "inception"
+
+  }
+
+
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+  request(queryUrl, function(error, response, body) {
 
       // If the request is successful
-      if (!error && response.statusCode === 200) {
+    if (!error && response.statusCode === 200) {
+         var result = JSON.parse(body)  
+        // console.log(result)
+          console.log("Movie title: " + result.Title);
+          console.log("Release Year: " + result.Year);
+          console.log("imdb rating :" + result.imdbRating)
+          console.log(result.Ratings[1].Source, result.Ratings[1].Value )
+           console.log("country: " + result.Country); 
+           console.log("Language: " + result.Language); 
+          console.log("Rating: " + result.Rated);
+          console.log("plot: " + result.Plot);
+          console.log("Actors: " + result.Actors);
           
-          console.log("Movie title: " + JSON.parse(body).Title);
-          console.log("Release Year: " + JSON.parse(body).Year);
-          console.log("Rating: " + JSON.parse(body).Rated);
-          console.log("plot: " + JSON.parse(body).Plot);
-          console.log("Actors: " + JSON.parse(body).Actors);
+
           
       }
     });   
 }
 
-omdb()
+
+//getMovieInfo()
+
+
+
+function getSpotifySongs(){
+
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+
+  var songName = stringBuilder()
+   
+  spotify.search({ type: 'track', query: songName }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+     
+    for (var i = 0; i< data.tracks.items.length; i++){
+      console.log(data.tracks.items[i].artists[0].name); 
+      console.log(data.tracks.items[i].name); 
+      console.log(data.tracks.items[i].external_urls.spotify); 
+      console.log(data.tracks.items[i].album.name); 
+      console.log("_______________________________")
+    }
+
+
+  });
+
+}
+
+function stringBuilder(){
+  var returnString = ""
+  var nodeArgs = process.argv
+  for (var i = 3; i < nodeArgs.length; i++) {
+      if (i > 3 && i < nodeArgs.length) {
+        returnString = returnString + "+" + nodeArgs[i];
+      }
+      else {
+        returnString += nodeArgs[i];
+      }
+  }
+  return returnString
+}
+
+//getSpotifySongs()
+
+
+switch(process.argv[2]){
+
+  case "my-tweets":
+    getTweets()
+    break;
+  case "spotify-this-song":
+    getSpotifySongs()
+    break
+  case "movie-this":
+    getMovieInfo()
+    break 
+
+}
